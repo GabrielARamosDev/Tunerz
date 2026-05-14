1efcaa3d133aa085b6cc135f7f8d46b16168ab69
GabrielARamosDev <gabrihot97@hotmail.com>
2026-05-14 01:06:31 -0300

    feat: add engine parts migration with piston and camshaft specifications; create user vehicle engines migration

--- backend/database/migrations/2026_02_05_034352_create_engine_parts_table.php
+++ backend/database/migrations/2026_02_05_034352_create_engine_parts_table.php
@@ -14,6 +14,59 @@ return new class extends Migration
         Schema::create('engine_parts', function (Blueprint $table) {
             $table->id();
 
+            $table->enum('piston_head_type', [
+
+            ]);
+            $table->enum('piston_head_material', [
+
+            ]);
+            $table->enum('piston_conrod_type', [
+
+            ]);
+            $table->enum('piston_conrod_material', [
+
+            ]);
+            $table->enum('piston_bore_mm', [
+
+            ]);
+            $table->enum('piston_stroke_mm', [
+
+            ]);
+
+            $table->enum('camshaft_type', [
+
+            ]);
+            $table->enum('camshaft_material', [
+
+            ]);
+
+            $table->enum('valve_control_type', [
+
+            ]);
+            $table->enum('valve_control_material', [
+
+            ]);
+
+            $table->enum('valve_type', [
+
+            ]);
+            $table->enum('valve_material', [
+
+            ]);
+            
+            $table->enum('intake_valve_diameter_mm', [
+
+            ]);
+            $table->enum('intake_valve_seat_angle', [
+
+            ]);
+            $table->enum('exhaust_valve_diameter_mm', [
+
+            ]);
+            $table->enum('exhaust_valve_seat_angle', [
+
+            ]);
+
             $table->boolean('has_VVT');
             $table->boolean('has_VVL');
             
